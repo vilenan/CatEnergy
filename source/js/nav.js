8213643c17e1acbce4b00290fa5915d108341418
Vilenan <vilenan@mail.ru>
2023-05-26 14:17:16 +0300

    исправила стили навигации

--- source/js/nav.js
+++ source/js/nav.js
@@ -3,6 +3,18 @@ const navigation = document.querySelector('.navigation');
 const body = document.querySelector('.page__body');
 const overlay = document.querySelector('.navigation__overlay');
 const menuItems = document.querySelectorAll('.navigation__item');
+const header = document.querySelector('.page-header');
+
+const headerHeight = header.offsetHeight;
+console.log(headerHeight);
+const windowHeight = document.documentElement.offsetHeight;
+console.log(windowHeight);
+const delta = windowHeight - headerHeight;
+const demension = document.documentElement.offsetWidth;
+
+if(demension < 768) {
+  navigation.setAttribute('style', `height : ${delta}px`);
+}
 
 function onEscPress(evt){
   if (evt.keyCode === 27) {
