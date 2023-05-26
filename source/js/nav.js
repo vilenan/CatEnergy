7b823b50ffb7e0a818b34e904f616489df3a940e
Vilenan <vilenan@mail.ru>
2023-05-26 11:39:51 +0300

    добавила overlay

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,10 +1,33 @@
 const burger = document.querySelector('.burger');
 const navigation = document.querySelector('.navigation');
 const body = document.querySelector('.page__body');
+const overlay = document.querySelector('.navigation__overlay');
+const menuItems = document.querySelectorAll('.navigation__item');
+
+function onEscPress(evt){
+  if (evt.keyCode === 27) {
+    burger.classList.remove('burger--active');
+    navigation.classList.remove('navigation--visible');
+    body.classList.remove('no-scroll');
+  }
+}
 
 burger.addEventListener('click', function () {
   burger.classList.toggle('burger--active');
   navigation.classList.toggle('navigation--visible');
-  body.classList.toggle('no-scroll')
+  body.classList.toggle('no-scroll');
+});
 
-})
+overlay.addEventListener('click', () => {
+  navigation.classList.remove('navigation--visible');
+});
+
+menuItems.forEach(el => {
+  el.addEventListener('click', () => {
+    burger.classList.remove('burger--active');
+    navigation.classList.remove('navigation--visible');
+    body.classList.remove('no-scroll');
+  })
+});
+
+document.addEventListener(`keydown`, onEscPress);
